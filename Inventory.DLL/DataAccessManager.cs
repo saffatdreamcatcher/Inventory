e9d401f9b17975a9d96f5c6e9ce9fd563363c541
sabbir <khandokarsabbir@gmail.com>
2023-01-28 21:56:16 -0600

    DataAccessManager Added

---
+++ Inventory.DLL/DataAccessManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Inventory.DLL
+{
+    public class DataAccessManager
+    {
+        private SqlConnection connection;
+
+        private SqlTransaction sqlTransaction;
+        public DataAccessManager() 
+        { 
+
+        }
+
+        public DataAccessManager(SqlConnection conn)
+        {
+            connection = conn;
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            if(connection == null)
+            {
+                var myConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+                connection = new SqlConnection(myConnectionString);
+            }
+            return connection;
+        }
+        
+        public void OpenTransaction()
+        {
+            if(sqlTransaction == null)
+            {
+                OpenConnection();
+                sqlTransaction = connection.BeginTransaction();
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            if (sqlTransaction != null)
+            {
+                sqlTransaction.Commit();
+                sqlTransaction.Dispose();
+                sqlTransaction = null;
+            }
+            CloseConnection();
+        }
+
+        public void RollbackTransaction()
+        {
+            if (sqlTransaction != null)
+            {
+                sqlTransaction.Rollback();
+                sqlTransaction.Dispose();
+                sqlTransaction = null;
+            }
+            CloseConnection();
+        }
+
+        public void OpenConnection()
+        {
+            GetSqlConnection();
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection != null)
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                    connection = null;
+                }
+            }
+        }
+    }
+}
